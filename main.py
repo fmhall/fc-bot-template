3ca1e98733a7ee32138331657f3a125c2fcad24e
Mason Hall <masonhall@gmail.com>
2023-02-16 12:59:39 -0500

    updating template to latest farcaster-py version

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import os
-from farcaster import MerkleApiClient
+from farcaster import Warpcast
 # from farcaster.models import Parent
 
+# The client will automagically cycle through access tokens
+client = Warpcast(os.environ.get("MNEMONIC"), rotation_duration=1)
 
-if __name__ == "__main__":
-    fcc = MerkleApiClient(os.environ.get("MNEMONIC"))
-    
-    # The API client will automatically cycle through access tokens
-
-    # Example request
-    print(fcc.get_me())
-
-
-    # Your code here
-    
-    # Like this:
-
-    for cast in fcc.stream_casts(max_counter=120):
-        print(cast)
-        if cast and "foo" in cast.text:
-            # fcc.post_cast("bar", parent=Parent(fid=cast.author.fid, hash=cast.hash))
-            pass
+# Stream new casts, and if one contains "foo", reply "bar"
+for cast in client.stream_casts():
+    if cast:
+        print(cast.author.username, ":")
+        print(cast.text)
+        print("----")
+        # fcc.post_cast("bar", parent=Parent(fid=cast.author.fid, hash=cast.hash))
+        pass
